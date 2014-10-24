fc8112ccf85b8469e2567e48589743987a90d2ba
suljkic <j.suljkic@gmail.com>
2014-10-24 18:19:02 +0200

    Added splitter for training and testing
    run as:
    java CorpusPreprocess nrLinesToLearn nrLinesToTest

--- CorpusPreprocess.java
+++ CorpusPreprocess.java
@@ -20,49 +20,106 @@ public class CorpusPreprocess {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//args[0] -> Amount of lines to preprocess for learning
+		//args[1] -> Amount of lines to preprocess for testing
+		
 		// TODO Auto-generated method stub
 		BufferedReader br;
 		BufferedWriter bw;
+		BufferedWriter bwT;
 		StringBuffer sb = new StringBuffer();
+		int nrLines=0;
+		int toLearn=Integer.parseInt(args[0]);
+		int toTest=Integer.parseInt(args[1]);
+		
 		try {
 			br = new BufferedReader(new FileReader("corpus.txt"));
 			bw = new BufferedWriter(new FileWriter("ppCorpus.txt"));
+			bwT = new BufferedWriter(new FileWriter("testSentences.txt"));
 			String line;
 			char[] lc;
+			
+			//Go trough the corpus and count the amount of lines present
+			while ((line=br.readLine()) != null) {
+				nrLines++;
+			}
+			br.close();
+			
+			if((toLearn+toTest)>nrLines){
+				System.err.println("Request invalid: Number of lines requested > nr of lines available in corpus.");
+				return;
+			}
+			
+			br=new BufferedReader(new FileReader("corpus.txt"));
 			
 			//Read a line from file (as long as there are lines in the file)
 			//Process the line
 			//Write the result to output file.
+			int current =0;
+			boolean testing =false;
 			while ((line=br.readLine()) != null) {
+				if(current==toLearn+1){
+					testing=true;
+				}
+				if(current==(toLearn+toTest)){
+					break;
+				}
 				lc = line.toCharArray();
 				for(char c : lc){
 					if(c=='.'){
-						sb.append(" .");
+						if(testing){
+							sb.append(" ");
+						}
+						else{
+							sb.append(" .PERIOD");
+						}
 					}
 					else if(c=='!'){
-						sb.append(" !");
-					}
+						if(testing){
+							sb.append(" ");
+						}
+						else{
+							sb.append(" !EXCL");
+						}					}
 					else if(c=='?'){
-						sb.append(" ?");
+						if(testing){
+							sb.append(" ");
+						}
+						else{
+							sb.append(" ?QMARK");
+						}
 					}
 					else if(c==','){
-						sb.append(" ,");
+						if(testing){
+							sb.append(" ");
+						}
+						else{
+							sb.append(" ,COMMA");
+						}
 					}
-					else if(c==' '){
-						sb.append(' ');
-						sb.append((char) 7);
-						sb.append(' ');
-					}
+//					else if(c==' '){
+//						sb.append(' ');
+//					}
 					else{
 						sb.append(c);
 					}
 				}
-				bw.write(sb.toString());
-				sb = new StringBuffer();
-				bw.newLine();
+				if(testing){
+					bwT.write(sb.toString());
+					sb = new StringBuffer();
+					bwT.newLine();
+				}
+				else{
+					bw.write(sb.toString());
+					sb = new StringBuffer();
+					bw.newLine();
+				}
+				
+				current++;
 			}
 			br.close();
 			bw.close();
+			bwT.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
