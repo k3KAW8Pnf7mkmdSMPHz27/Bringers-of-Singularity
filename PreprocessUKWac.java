fea4c38d0cba5123f99f13936942329fa314ffd5
Jonatan Asketorp <jonatan@asketorp.com>
2014-11-02 06:53:10 +0100

    minor

--- PreprocessUKWac.java
+++ PreprocessUKWac.java
@@ -2,6 +2,6 @@ public class PreprocessUKWac {
     public static void main(String[] args) {
         String ukwacStringPath = "UKWAC-1.xml";
         ukWacToTxt.main(new String[]{ukwacStringPath});
-        CorpusPreprocess.main(null);
+        CorpusPreprocess.main(new String[]{"10000", "10000"});
     }
 }
