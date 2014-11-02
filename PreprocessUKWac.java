207fc62b49dff25b404573452f880ee920b69032
Jonatan Asketorp <jonatan@asketorp.com>
2014-11-02 17:42:09 +0100

    meh

--- PreprocessUKWac.java
+++ PreprocessUKWac.java
@@ -1,7 +1,7 @@
 public class PreprocessUKWac {
     public static void main(String[] args) {
         String ukwacStringPath = "UKWAC-1.xml";
-        //ukWacToTxt.main(new String[]{ukwacStringPath});
-        CorpusPreprocessUK.main(new String[]{"100000", "10000", "1"});
+        ukWacToTxt.main(new String[]{ukwacStringPath});
+        CorpusPreprocessUK.main(new String[]{"50000", "1000"});
     }
 }
